177c42011fa9d2d4b80a85259ec5465849b93731
Troestler Markus <trm4wi@bosch.com>
2026-04-29 15:49:34 +0200

    adjust build script for startup image and icons

--- src/build_noconsole.bat
+++ src/build_noconsole.bat
@@ -1,1 +1,1 @@
-pyinstaller --noconfirm --noconsole --icon=icon.ico --hidden-import="sklearn.utils._typedefs" --add-data "config.json;." --add-data "startup.png;." --distpath "../dist" --workpath "../build" --name hyperlyse main.py
+pyinstaller --noconfirm --noconsole --icon=icon.ico --hidden-import="sklearn.utils._typedefs" --add-data "config.json;." --add-data "startup.png;." --add-data "icons;icons" --distpath "../dist" --workpath "../build" --name hyperlyse main.py
